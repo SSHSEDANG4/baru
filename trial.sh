9d44559178fda99f66280e7d2bd0095b450731ed
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-22 19:38:50 +0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -5,6 +5,7 @@ NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 
 IP=$(wget -qO- ifconfig.me/ip);
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -27,10 +28,13 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Informasi Trial SSH & OpenVPN"
+echo -e "Name : Trial SSH , WebSocket, Openvpn"
+echo -e "===============================" | lolcat
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
-echo -e "==============================="
+echo -e "Expired On     : $exp"
+echo -e "===============================" | lolcat
+echo -e "ISP            : $ISP"
 echo -e "Host           : $IP"
 echo -e "OpenSSH        : 22"
 echo -e "Dropbear       : 109, 143"
@@ -40,6 +44,11 @@ echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-$ovpn.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-$ovpn2.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "badvpn         : 7100-7300"
-echo -e "==============================="
-echo -e "Expired On     : $exp"
-echo -e "Script Install  : RPJ WONOSOBO"
+echo -e "===============================" | lolcat
+echo -e "PAYLOAD WS     :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "PAYLOAD WS SSL :"
+echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "Script By SSH SEDANG NETWORK"
