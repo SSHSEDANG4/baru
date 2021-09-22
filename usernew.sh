cc5610f5d2bd2f2f2d478a52cafd226dd3c347c7
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-22 19:31:18 +0700

    Update usernew.sh

--- usernew.sh
+++ usernew.sh
@@ -19,6 +19,7 @@ read -p "Password : " Pass
 read -p "Expired (hari): " masaaktif
 
 IP=$(wget -qO- ifconfig.me/ip);
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
@@ -39,11 +40,13 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Thank You For Using Our Services"
-echo -e "SSH & OpenVPN Account Info"
+echo -e "Name : SSH , WebSocket, Openvpn"
+echo -e "===============================" | lolcat
 echo -e "Username       : $Login "
 echo -e "Password       : $Pass"
-echo -e "==============================="
+echo -e "Expired On      : $exp"
+echo -e "===============================" | lolcat
+echo -e "ISP            : $ISP"
 echo -e "Domain         : $domain"
 echo -e "Host           : $IP"
 echo -e "OpenSSH        : 22"
@@ -55,12 +58,11 @@ echo -e "OpenVPN        : TCP $ovpn http://$IP:81/client-tcp-1194.ovpn"
 echo -e "OpenVPN        : UDP $ovpn2 http://$IP:81/client-udp-2200.ovpn"
 echo -e "OpenVPN        : SSL 442 http://$IP:81/client-tcp-ssl.ovpn"
 echo -e "badvpn         : 7100-7300"
-echo -e "==============================="
-echo -e "PAYLOAD"                                                          
-echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Connection: Keep-Alive[crlf]User-Agent: [ua][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "==============================="
-echo -e "SETING HOST SSH"               
-echo -e "bugisisendiri:2082@$Login:$Pass"
-echo -e "==============================="
-echo -e "Script Install  : "
-echo -e "Expired On      : $exp"
+echo -e "===============================" | lolcat
+echo -e "PAYLOAD WS     :"
+echo -e "GET / HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "PAYLOAD WS SSL :"
+echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
+echo -e "===============================" | lolcat
+echo -e "Script By SSH SEDANG NETWORK"
