adcc0093e01e687c32ce36858ca60134632e5cba
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-22 23:26:28 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -40,8 +40,7 @@ sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 
 # Buat config client TCP 1194
 cat > /etc/openvpn/client-tcp-1194.ovpn <<-END
-############## WELCOME TO HideSSH.com ###############
-########## Config Bye HideSSH ###########
+########## SSH SEDANG NETWORK ###########
 ####### DONT FORGET TO SUPPORT US #######
 client
 dev tun
@@ -67,8 +66,7 @@ sed -i $MYIP2 /etc/openvpn/client-tcp-1194.ovpn;
 
 # Buat config client UDP 2200
 cat > /etc/openvpn/client-udp-2200.ovpn <<-END
-############## WELCOME TO HideSSH.com ###############
-########## Config Bye HideSSH ###########
+########## SSH SEDANG NETWORK ###########
 ####### DONT FORGET TO SUPPORT US #######
 client
 dev tun
@@ -94,8 +92,7 @@ sed -i $MYIP2 /etc/openvpn/client-udp-2200.ovpn;
 
 # Buat config client SSL
 cat > /etc/openvpn/client-tcp-ssl.ovpn <<-END
-############## WELCOME TO HideSSH.com ###############
-########## Config Bye HideSSH ###########
+########## SSH SEDANG NETWORK ###########
 ####### DONT FORGET TO SUPPORT US #######
 client
 dev tun
